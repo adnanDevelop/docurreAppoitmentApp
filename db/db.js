5932c30c849751dd1be21505400d413da1457752
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-10-28 16:40:38 +0500

    removed code from db

--- db/db.js
+++ db/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000, // Sets a 5-second timeout
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected with mongodb");
   } catch (error) {
     console.error(`Error while connecting with database: ${error.message}`);
